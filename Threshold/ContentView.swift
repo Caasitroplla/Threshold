5a3d7a730fa7f03ea6353a078742f7760b51acce
isaac <caasitroplla@icloud.com>
2025-08-26 09:44:45 +0100

    Initial Commit

---
+++ Threshold/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Threshold
+//
+//  Created by Issac Allport on 26/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Threshold/ThresholdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThresholdApp.swift
+//  Threshold
+//
+//  Created by Issac Allport on 26/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct ThresholdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
